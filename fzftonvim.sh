8e7602103eaa900b6d9c5afb42763328d048164b
obscurityUser <145868864+obscurityUser@users.noreply.github.com>
2023-09-28 19:17:22 +0100

    Update fzftonvim.sh

--- fzftonvim.sh
+++ fzftonvim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-FILE=$(rg --hidden -l "" | fzf)
+FILE=$(rg --hidden -l "" -g '!.local/' -g '!.mozilla/' -g '!.librewolf/' -g '!.npm/' -g '!.android/' -g '!.cargo/' -g '!.java/' -g '!.pki/' -g '!.vscode/' -g '!.vscode-oss/' -g '!.cache/' | fzf)
 nvim $FILE
